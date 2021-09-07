3d4dcc079e3cddd6a4013e8e0a6f3a66511b0abb
Dimitris Sarlis <dimitris@dfinity.org>
2021-09-07 15:58:02 +0200

    Added rust stubs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,2 +1,169 @@
-#[export_name = "canister_init"]
+use ic_cdk::export::candid::{CandidType, Deserialize, Principal};
+use ic_cdk_macros::{query, update};
+use serde_bytes::ByteBuf;
+use std::collections::{HashMap, HashSet};
+
+type AnswerId = u64;
+type Content = ByteBuf;
+type Duration = u64;
+type TaskId = u64;
+type TaskPayload = ByteBuf;
+type Timestamp = u64;
+type Amount = u64;
+
+struct State {
+    _tasks: HashMap<TaskId, TaskInternal>,
+    _answers: HashMap<AnswerId, Answer>,
+    _ledger: HashMap<Principal, Amount>,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State {
+            _tasks: HashMap::default(),
+            _answers: HashMap::default(),
+            _ledger: HashMap::default(),
+        }
+    }
+}
+
+thread_local! {
+    static STATE: State = State::default();
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+enum Choice {
+    #[serde(rename = "yes")]
+    Yes,
+    #[serde(rename = "no")]
+    No,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+enum Language {
+    #[serde(rename = "chinese")]
+    Chinese,
+    #[serde(rename = "french")]
+    French,
+    #[serde(rename = "german")]
+    German,
+    #[serde(rename = "greek")]
+    Greek,
+    #[serde(rename = "hindi")]
+    Hindi,
+    #[serde(rename = "italian")]
+    Italian,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+enum TaskType {
+    #[serde(rename = "translate_text")]
+    TranslateText,
+    #[serde(rename = "edit_image")]
+    EditImage,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+enum TaskStatus {
+    #[serde(rename = "open")]
+    Open,
+    #[serde(rename = "closed")]
+    Closed,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct TranslateTextInput {
+    input: String,
+    language: Language,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct Answer {
+    submitter: Principal,
+    submission_time: Timestamp,
+    votes: Vec<Vote>,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct Task {
+    submitter: Principal,
+    task_type: TaskType,
+    payload: TaskPayload,
+    deadline: Timestamp,
+    reward: Amount,
+    answers: Vec<Answer>,
+    status: TaskStatus,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct ShortTask {
+    id: TaskId,
+    submitter: Principal,
+    task_type: TaskType,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct Vote {
+    voter: Principal,
+    choice: Choice,
+}
+
+#[allow(dead_code)]
+struct TaskInternal {
+    submitter: Principal,
+    task_type: TaskType,
+    payload: TaskPayload,
+    deadline: Timestamp,
+    reward: Amount,
+    answers: HashSet<AnswerId>,
+    status: TaskStatus,
+}
+
+#[update]
+fn register() {}
+
+#[update]
+fn submit_task(
+    _task_type: TaskType,
+    _payload: TaskPayload,
+    _duration: Duration,
+    _reward: Amount,
+) -> TaskId {
+    0
+}
+
+#[query]
+fn get_task(_id: TaskId) -> Task {
+    Task {
+        submitter: Principal::anonymous(),
+        task_type: TaskType::TranslateText,
+        payload: ByteBuf::from(vec![]),
+        deadline: 0,
+        reward: 0,
+        answers: vec![],
+        status: TaskStatus::Open,
+    }
+}
+
+#[query]
+fn get_all_tasks() -> Vec<ShortTask> {
+    vec![]
+}
+
+#[query]
+fn get_balance() -> Amount {
+    0
+}
+
+#[update]
+fn answer_task(_id: TaskId, _content: Content) -> AnswerId {
+    0
+}
+
+#[update]
+fn vote(_id: AnswerId, _choice: Choice) {}
+
+#[export_name = "canister_heartbeat"]
+fn hearbeat() {}
+
 fn main() {}
